e2f3ddcd58ccecc0282b3f1a21411e22f2db4e77
Dakshithas <59331614+Dakshithas@users.noreply.github.com>
2020-03-25 12:16:14 +0530

    Create home.php

---
+++ home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            HOME
+        </title>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    </head>
+    <body>
+        <h1>Lady Ridgeway Hospital For Children <br>
+        <span class="sinhala">රිජ්වේ ආර්යා ළමා රෝහල</span>
+        </h1>
+        <h1> </h1>
+        <ul>
+            <li><a href="stafflogin.php">Log In - for staff</a></li>
+            <li><a href="patientlogin.php">Log In - for patient</a></li>
+        </ul>
+    </body>
+</html>
